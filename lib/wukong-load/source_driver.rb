9502d087fce77db590ff9678bdb8a5bf0715509e
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-01-14 00:30:45 -0600

    override process method of StdioDriver to flush on each write

--- lib/wukong-load/source_driver.rb
+++ lib/wukong-load/source_driver.rb
@@ -21,6 +21,17 @@ module Wukong
         receive_line(index.to_s)
         self.index += 1
       end
+
+      # :nodoc:
+      #
+      # Not sure why I have to add the call to $stdout.flush at the
+      # end of this method.  Supposedly $stdout.sync is called during
+      # the #setup method in StdoutProcessor in
+      # wukong/widget/processors.  Doesn't that do this?
+      def process record
+        $stdout.puts record
+        $stdout.flush
+      end
       
     end
   end
