c53b499e7d3e0ec373c330c609f9878b9af77e94
Dhruv Bansal <dhruv@ph.utexas.edu>
2013-03-16 00:19:48 -0500

    cleaned up loader, use explicitly timestamp keys, not implicit regexing of timestamp values, default collection key is now _type

--- lib/wukong-load/loaders/mongodb.rb
+++ lib/wukong-load/loaders/mongodb.rb
@@ -8,15 +8,17 @@ module Wukong
     # Uses the 'mongo' gem to connect and write data.
     #
     # Allows loading records into a given database and collection.
-    # Records can have fields `_database` and `_collection` which
-    # override the given database and collection on a per-record
-    # basis.
+    # Records can have fields `_database` and `_type` which override
+    # the given database and collection on a per-record basis.
     #
     # Records can have an `_id` field which indicates an update, not
     # an insert.
     #
     # The names of these fields within each record (`_database`,
-    # `_collection`, and `_id`) can be customized.
+    # `_type`, and `_id`) can be customized.
+    #
+    # Keys listed in the `timestamp_keys` field will be parsed into
+    # Time objects before they are written to MongoDB.
     class MongoDBLoader < Loader
 
       field :host,             String, :default => 'localhost', :doc => "MongoDB host"
@@ -26,6 +28,7 @@ module Wukong
       field :database_field,   String, :default => '_database', :doc => "Name of field in each record overriding default MongoDB database"
       field :collection_field, String, :default => '_type', :doc => "Name of field in each record overriding default MongoDB collection"
       field :id_field,         String, :default => '_id', :doc => "Name of field in each record providing ID of existing MongoDB record to update"
+      field :timestamp_keys,   Array,  :default => %w[created_at updated_at], doc: "Keys with values that will be parsed into Time instances before writing to MongoDB"
 
       description <<-EOF.gsub(/^ {8}/,'')
         Loads newline-separated, JSON-formatted records over STDIN
@@ -37,8 +40,8 @@ module Wukong
         local MongoDB server.
 
         Input records will be written to a default database and
-        collection.  Each record can have _database and _collection
-        fields to override this on a per-record basis.
+        collection.  Each record can have _database and _type fields
+        to override this on a per-record basis.
 
         Records with an _id field will be trigger updates, the rest
         inserts.
@@ -46,7 +49,7 @@ module Wukong
         All other fields within a record are assumed to be the names
         of actual columns in the table.
 
-        The fields used (_index, _collection, and _id) can be changed:
+        The fields used (_index, _type, and _id) can be changed:
 
           $ cat data.json | wu-load mongodb --host=10.123.123.123 --database=web_events --collection=impressions --id_field=impression_id
       EOF
@@ -72,22 +75,75 @@ module Wukong
 
       # Load a single record into MongoDB.
       #
-      # If the record has an ID, we'll issue an update, otherwise an
+      # If the record contains the key `_delete` then issue a remove
+      # request.  Otherwise insert or update the record.
+      #
+      # @param [Hash] record
+      def load record
+        case
+        when record["_delete"]
+          delete(record)
+        else
+          create_or_update(record)
+        end
+      end
+
+      # Create or update a single record in MongoDB.
+      #
+      # If `record` has an ID, we'll issue an update, otherwise an
       # insert.
       #
-      # @param [record] Hash
-      def load record
-        id = id_for(record)
+      # @param [Hash] record
+      def create_or_update record
+        db_name   = database_name_for(record)
+        coll_name = collection_name_for(record)
+        id        = id_for(record)
+        pointer   = "/#{db_name}/#{coll_name}/#{id}"
         if id
           res = collection_for(record).update({:_id => id}, interpret(record), :upsert => true)
-          if res['updatedExisting']
-            log.info("Updated #{id}")
+          case
+          when res['error']
+            log.error(res['error'])
+          when res['updatedExisting']
+            log.debug("Updated #{pointer}")
           else
-            log.info("Inserted #{id}")
+            log.debug("Inserted #{pointer}")
           end
         else
           res = collection_for(record).insert(interpret(record))
-          log.info("Inserted #{res}")
+          log.info("Inserted #{pointer}#{res}")
+        end
+      end
+
+      # Remove a record from MongoDB.
+      #
+      # If `record` has an ID field (as defined by #id_for) then
+      # delete the record with the given ID from MongoDB.
+      #
+      # Otherwise, use the value of the `_delete` key as a query to
+      # select records to delete.
+      #
+      # @param [Hash] record
+      def delete record
+        db_name   = database_name_for(record)
+        coll_name = collection_name_for(record)
+        case
+        when id = id_for(record)
+          pointer   = "/#{db_name}/#{coll_name}/#{id}"
+          res = collection_for(record).remove({:_id => id})
+          if res["err"]
+            log.error(res["err"])
+          else
+            log.debug("Deleted #{pointer}")
+          end
+        when query = delete_query_for(record)
+          pointer   = "/#{db_name}/#{coll_name}"
+          res = collection_for(record).remove(query)
+          if res["err"]
+            log.error(res["err"])
+          else
+            log.debug("Deleted #{res['n']} records from #{pointer}")
+          end
         end
       end
 
@@ -108,7 +164,7 @@ module Wukong
 
       # :nodoc:
       def collection_name_for record
-        record.delete(collection_field) || self.collection
+        record[collection_field] || self.collection
       end
 
       # :nodoc:
@@ -119,12 +175,16 @@ module Wukong
       def interpret record
         Hash[record.map do |key, value|
           case
-          when value.is_a?(String) && value =~ /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}-\d{2}:\d{2}$/
+          when timestamp_keys.include?(key.to_s)
             [key, Time.parse(value)] rescue [key, value]
           else
             [key, value]
           end
         end]
+      end
+
+      def delete_query_for record
+        record["_delete"] || {}
       end
       
       register :mongodb_loader
